350672575716b057ac2bd2094081235d423a430d
Hameed Wah <hamwah@Hameeds-MacBook-Air.local>
2023-07-05 14:52:58 -0700

    added search button, removed view news button

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 $(document).ready(() => {
-	
-
-
 	//function for fetching 2 api req
 	function fetchData() {
-		const newsURL = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,earnings&apikey=FKGNY01DWDV4G28G'
-		// const stocksListURL = 'https://api.twelvedata.com/stocks'
+		const newsURL =
+			'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=technology,earnings&apikey=FKGNY01DWDV4G28G'
 		const apiKey = '63ee659aa58b484fb18a38bd3a045443'
-		const stocksSymbols = ['AAPL', 'GOOGL', 'BRK.A']
-		const fetchStockData = (symbol) => {
-			const stocksPricesURL = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=1h&apikey=${apiKey}`
-			return fetch(stocksPricesURL).then((res) => res.json())
-		}
+		const stocksSymbols = ['AAPL', 'TSLA', 'NKE']
 
+		const fetchStockData = async (symbol) => {
+			const stocksPricesURL = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=1h&apikey=${apiKey}`
+			const res = await fetch(stocksPricesURL)
+			return await res.json()
+		}
+		//fetching stocks and news from url
 		fetch(newsURL)
 			.then((res) => res.json())
 			.then((newsData) => {
@@ -44,24 +43,12 @@ $(document).ready(() => {
 			const symbol = meta.symbol
 			const values = stocksPricesData.values
 
-			// console.log(values);
-
-			const cardElement = $(`<div class="card">`)
+			const cardElement = $('<div class="card">')
 			const cardBody = $('<div class="card-body">')
 
-			// Retrieve opening and closing prices from the values array
 			if (values && values.length > 0) {
 				const openingPrice = Number(values[0].open).toFixed(2)
 				const closingPrice = Number(values[values.length - 1].close).toFixed(2)
-
-				const priceChange = closingPrice - openingPrice
-
-				//news button for each stock container
-				const newsButton = $(
-					`<button class="btn btn-primary btn-news" data-symbol="${symbol}">View News</button>`
-				)
-
-				// card body for stocks container
 
 				cardBody.append(`<h5 class="card-title">${symbol}</h5>`)
 				cardBody.append(
@@ -70,8 +57,8 @@ $(document).ready(() => {
 				cardBody.append(
 					`<p class="card-text">Closing Price: ${closingPrice}</p>`
 				)
+				const priceChange = closingPrice - openingPrice
 
-				//formatting
 				if (priceChange > 0) {
 					cardElement.addClass('stock-up')
 					cardBody.addClass('text-success')
@@ -80,14 +67,18 @@ $(document).ready(() => {
 					cardBody.addClass('text-danger')
 				}
 
-				// Append the card body to the card
 				cardElement.append(cardBody)
-				// Append newsButton
-				cardElement.append(newsButton)
-				// Append the card to the stocks container
 				stocksContainer.append(cardElement)
 			}
 		})
+	}
+
+	// function to fetch stock data for a given symbol
+	const fetchStockDataBySymbol = async (symbol) => {
+		const apiKey = '63ee659aa58b484fb18a38bd3a045443'
+		const stocksPricesURL = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=1h&apikey=${apiKey}`
+		const res = await fetch(stocksPricesURL)
+		return await res.json()
 	}
 
 	//function to display news
@@ -95,7 +86,7 @@ $(document).ready(() => {
 		const newsContainer = $('#newsContainer')
 		// console.log(newsData)
 
-		// for loop to iterate over articles and create html for articles
+		// for loop to iterate over articles and create html card for articles
 		for (let i = 0; i < newsData.length; i++) {
 			const feed = newsData[i]
 			const title = feed.title
@@ -107,7 +98,7 @@ $(document).ready(() => {
 				<h3>${title}</h3>
 				<p>Source: ${source}</p>
 				<p>
-					<a href="${url}" target="_blank">Read More</a>
+					<a href="${url}">Read More</a>
 				</p>
 				</div>`)
 
@@ -116,34 +107,43 @@ $(document).ready(() => {
 			newsContainer.append(articleElement)
 		}
 	}
-
+	
+	//portfolioLink using .hide and .show to navigate pages
 	$('#portfolioLink').click(() => {
 		$('#newsContainer').hide()
 		$('#portfolioContent').show()
 		$('#stocksContainer').show()
 	})
 
+	//latestNews using .hide and .show to navigate pages
 	$('#latestNews').click(() => {
 		$('#portfolioContent').hide()
 		$('#newsContainer').show()
 		$('#stocksContainer').hide()
 	})
 
-	$('#newsContainer').on('click', '.btn-news', (event) => {
-		const symbol = $(event.currentTarget).data('symbol')
-		fetchNews(symbol)
+	//searchButton - takes input, fetches it using fetchStockDataBySymbol function and renders data via displayStocks function
+	$('#searchButton').on('click', () => {
+		const stockSymbol = $('#stockInput').val().toUpperCase()
+		$('#stockInput').val('')
+		if (stockSymbol) {
+			fetchStockDataBySymbol(stockSymbol)
+				.then((stockData) => {
+					const stocksPricesDataArray = [stockData]
+					displayStocks(stocksPricesDataArray)
+				})
+				.catch((error) => {
+					console.log(error)
+				})
+		}
 	})
-	
-
-})
+});
 
 
 
 
 
-
-
-
+// const stocksListURL = 'https://api.twelvedata.com/stocks'
 // function fetchNews() {
 	// 	const apiKey = 'FKGNY01DWDV4G28G';
 	// 	const newsAPI = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=earnings&apikey=FKGNY01DWDV4G28G';
@@ -157,3 +157,16 @@ $(document).ready(() => {
 	// 			console.log(error)
 	// 		})
 	// }
+
+	//news button for each stock container
+	// const newsButton = $(
+	// 	`<button class="btn btn-primary btn-news" data-symbol="${symbol}">View News</button>`
+	// )
+
+	// $('#newsContainer').on('click', '.btn-news', (event) => {
+	// 	const symbol = $(event.currentTarget).data('symbol')
+	// 	fetchNews(symbol)
+	// })
+
+	// Append newsButton
+	// cardElement.append(newsButton)
